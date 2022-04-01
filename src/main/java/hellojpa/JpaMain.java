73bdd7a9975bcf8b04f80d8faa2fb10dfce109eb
iokim <iokim1220@gmail.com>
2022-04-01 19:23:56 +0900

    # 영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,26 +19,60 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloB");
-//            em.persist(member); // 저장
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ"); // 변경 감지 dirty check
+            // 1차캐시 안에 pk와 entity, 스냅샷이 있다(최초로 영속성 컨텍스트에 들어온 상태)
+            // tx.commit() 될때 내부적으로 flush 실행하면서 entity와 스냅샷을 비교 -> 변경이 있을 경우 쓰기 지연 SQL 저장소에 업데이트 쿼리 생성
 
-//            Member findMember = em.find(Member.class, 1L);
+            System.out.println("======================");
+            tx.commit(); // 커밋할때 DB에 쿼리 나감
+
+            ////////////////////////////////////////////////////////////////////
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("======================");
+//            tx.commit(); // 커밋할때 DB에 쿼리 나감
+
+            ////////////////////////////////////////////////////////////////////
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L); // 똑같은 걸로 두 번 조회하면 영속성 캐시에 있는걸 반환..
+//
+//            // == 비교 가능 동일성 보장, 1차캐시가 있기 떄문에 가능(REPEATABLE READ등급의 트랜잭션 격리 수준을 데이터베이스가 아닌 애플리케이션 차원에서 제공)
+//            System.out.println("(findMember1 == findMember2) = " + (findMember1 == findMember2));
+
+            ////////////////////////////////////////////////////////////////////
+            // 비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            // 영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); // 저장
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L); // 조회할 때 조회쿼리 안나감..
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
 //            findMember.setName("HelloJPA"); // 수정
 //            em.remove(findMember); // 삭제
 
+            ////////////////////////////////////////////////////////////////////
             // JPQL: 엔티티 객체를 대상으로 쿼리, SQL: 데이터베이스 테이블을 대상으로 쿼리
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    // 페이징(방언에 맞춰서 DB에 맞게 번역)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    // 페이징(방언에 맞춰서 DB에 맞게 번역)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
 
-            tx.commit();
+//            tx.commit();
         }catch (Exception e) {
             tx.rollback();
         }finally {

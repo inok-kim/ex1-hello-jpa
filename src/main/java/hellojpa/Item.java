ccd3f0d4bb32b352eb6735e569605d634a89161c
iokim <iokim1220@gmail.com>
2022-04-06 17:18:07 +0900

    # Mapped Superclass - 매핑 정보 상속

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-//@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.JOINED)
 //@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 단일 테이블 전략에서는 @DiscriminatorColumn 어노테이션이 없어도 DType 컬럼이 생성됨
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 추상 클래스로 만들어야함. 일반 클래스일 경우 Item 테이블 만들어짐, item클래스로 조회할 경우 union all로 조회해옴...
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // 추상 클래스로 만들어야함. 일반 클래스일 경우 Item 테이블 만들어짐, item클래스로 조회할 경우 union all로 조회해옴...
 @DiscriminatorColumn
 public abstract class Item {
 
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -14,21 +15,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbb");
-            movie.setName("바람과함께사라지다");
-            movie.setPrice(10000);
-            em.persist(movie);
 
-            em.flush();
-            em.clear();
+            Member member = new Member();
+            member.setUsername("user");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-//            Item item = em.find(Item.class, movie.getId());
-//            System.out.println("item = " + item);
+            em.persist(member);
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+//            Movie movie = new Movie();
+//            movie.setDirector("aaa");
+//            movie.setActor("bbb");
+//            movie.setName("바람과함께사라지다");
+//            movie.setPrice(10000);
+//            em.persist(movie);
+//
+//            em.flush();
+//            em.clear();
+//
+////            Item item = em.find(Item.class, movie.getId());
+////            System.out.println("item = " + item);
+//
+//            Movie findMovie = em.find(Movie.class, movie.getId());
+//            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         }catch (Exception e) {

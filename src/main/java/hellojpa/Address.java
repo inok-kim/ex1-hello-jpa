97b020f6a5434c6a65179fde2b0e47f6412d6420
iokim <iokim1220@gmail.com>
2022-04-08 14:53:44 +0900

    # 동일성 비교, 동등성 비교

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.Objects;
+
 public class Address {
 
     private String city;
@@ -26,4 +28,18 @@ public class Address {
     public String getZipcode() {
         return zipcode;
     }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Address)) return false;
+//        Address address = (Address) o;
+//        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+//    }
+//
+//    // equals 구현 시 hashCode도 같이 구현해줘야 hashCode 를 사용하는 hashMap 등에서 사용할 수 있다
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(city, street, zipcode);
+//    }
 }
